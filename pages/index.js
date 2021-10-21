5d04b7d128ca9bdb87c75bccadf95fcacd29b597
urmlhst <sitiurmilla27@gmail.com>
2021-10-21 22:27:48 +0700

    update index

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Mulai edit yuhuuu{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
